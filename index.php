fabb07af9ec65b78582e485a8f53fe8a377d0e99
adio87 <klymenko.viktor87@gmail.com>
2015-04-01 10:33:25 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+$db_hostname = 'localhost';
+$db_database = 'im_market_db';
+$db_username = 'root';
+$db_password = '1111';
+
+try {
+    $dbh = new PDO("mysql:host=$db_hostname;dbname=$db_database", $db_username, $db_password);
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+$dbh = null;
+
+
+
+
+
